f2f7363bad1065c2907828896aeebfd0ee45a16a
Eduardo Rocha <edu_a.r@hotmail.com>
2022-08-20 21:22:39 -0300

    cv pdf download

--- src/components/Home/Information/Curriculum/curriculum.js
+++ src/components/Home/Information/Curriculum/curriculum.js
@@ -5,6 +5,12 @@ function Curriculum( props ) {
 
     const { extraClass, skills } = props;
 
+    const cvUrl = '/assets/CV - Eduardo Almeida Rocha.pdf'
+
+    function downloadCV(){
+        window.open(cvUrl);
+    }
+
     return (
         <div className={`cv-wrapper`}>
             <div className={`skills-wrapper-${extraClass}`}>
@@ -22,7 +28,7 @@ function Curriculum( props ) {
             </div>
             <div className="cv">
                 <p>Para mais informações recomendo baixar meu CV</p>
-                <button className={`cv-download-${extraClass}`}>
+                <button className={`cv-download-${extraClass}`} onClick={downloadCV}>
                     <img src='./assets/images/download.svg' alt='baixar cv'/>
                     <span>Baixar CV</span>
                 </button>
